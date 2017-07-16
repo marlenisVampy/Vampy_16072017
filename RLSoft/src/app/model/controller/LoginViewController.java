98719e453feec35d5a1fda24218c259f3a416db0
Marlenis Concepcion <vampy1277@gmail.com>
2017-07-16 13:15:44 -0600

    Login.

--- RLSoft/src/app/model/controller/LoginViewController.java
+++ RLSoft/src/app/model/controller/LoginViewController.java
@@ -7,7 +7,10 @@ package app.model.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +18,12 @@ import javafx.fxml.Initializable;
  * @author PC
  */
 public class LoginViewController implements Initializable {
+ 
+
+@FXML private void verifyUserData(ActionEvent e)throws Exception
+    {
+        
+    }
 
     /**
      * Initializes the controller class.
@@ -23,5 +32,9 @@ public class LoginViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+   /* public void setStage(Stage stage) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }*/
+ 
 }
---
+++ RLSoft/src/app/tester/RLSoftFXMain.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.tester;
+
+
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import static javafx.scene.input.DataFormat.URL;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author PC
+ */
+public class RLSoftFXMain extends Application {
+    
+   
+   @Override
+    public void start(Stage stage)throws Exception {
+      Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+       Scene scene = new Scene(root);
+
+    stage.setScene(scene);
+    stage.setTitle("RLSoft-Login v1.0");
+    stage.setIconified(true);
+    stage.setResizable(false);
+    stage.show();
+        
+        }
+         
+    
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+ }
+    
